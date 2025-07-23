34fe7b9b03862639b87a58332278ab390ce233d0
y2code dev <y2codedev@gmail.com>
2025-07-23 13:58:39 +0530

    optmize image-compressor page

--- src/app/(root)/image-compressor/page.tsx
+++ src/app/(root)/image-compressor/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
                                         <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                         </svg>
-                                        Choose an Image
+                                        <span className='text-sm md:text-base font-medium'>Choose an Image</span>
                                     </>
                                 )}
                                 <input
@@ -108,7 +108,7 @@ export default function Home() {
                                     disabled={loading}
                                 />
                             </label>
-                            <p className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
+                            <p className="mt-2 sm:text-sm text-xs text-center text-gray-500 dark:text-gray-400">
                                 Supports JPG, PNG, WEBP (Max: 10MB)
                             </p>
                         </div>
