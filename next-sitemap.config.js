a6544e33d322581c4019df95c1651e997c720c83
y2code dev <y2codedev@gmail.com>
2025-07-29 11:19:07 +0530

    add seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://fast-save.vercel.app',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
+  trailingSlash: true,
 };
