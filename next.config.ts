c2e5175f3121ddcfc9e273161afb8ced7f365e51
y2code dev <y2codedev@gmail.com>
2025-07-29 11:24:11 +0530

    add seo

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
-const siteUrl = "https://fast-save.vercel.app";
+
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  trailingSlash: true,
-   async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/',
-        permanent: true, 
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
