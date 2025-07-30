7fc90cda25abe2d2aef624fbb87045e3fd49a4a4
y2code dev <y2codedev@gmail.com>
2025-07-30 12:26:23 +0530

    fix bugs of dark theme

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -30,14 +30,6 @@ const Navbar = () => {
     />
   )
 
-  if (resolvedTheme === 'dark') {
-    return <FaSun onClick={() => setTheme('light')} />
-  }
-
-  if (resolvedTheme === 'light') {
-    return <FaMoon onClick={() => setTheme('dark')} />
-  }
-
   return (
     <header className="sticky top-0 z-50 w-full bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700  transition-colors duration-300">
       <nav className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
