aa71d07cb5f4797978f2bc07b6a6edcedeba8ea7
y2code dev <y2codedev@gmail.com>
2025-07-31 13:44:06 +0530

    fix bugs of dark theme

--- src/components/sections/ReelResult.tsx
+++ src/components/sections/ReelResult.tsx
@@ -57,13 +57,13 @@ export default function ReelResult({ data, isSaving, setIsSaving }: ReelResultPr
                     <div className="flex-1 space-y-3">
                         <div>
                             <h4 className="text-sm font-medium text-gray-500 dark:text-gray-400">Title</h4>
-                            <p className="text-lg font-semibold text-gray-900 dark:text-white">
+                            <p className="text-lg  text-gray-900 dark:text-white line-clamp-2">
                                 {data?.title || 'Untitled Reel'}
                             </p>
                         </div>
                         <div>
-                            <h5 className="text-sm font-medium text-gray-500 dark:text-gray-400">Description</h5>
-                             <p className="text-lg font-semibold text-gray-900 dark:text-white line-clamp-5">
+                            <h4 className="text-sm font-medium text-gray-500 dark:text-gray-400">Description</h4>
+                             <p className="text-sm text-gray-400 dark:text-gray-100 line-clamp-6">
                                 {data?.description || 'Untitled Reel'}
                             </p>
                         </div>
