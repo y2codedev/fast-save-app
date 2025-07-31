d1d728685904bea09b58149bff0125d4fb271196
y2code dev <y2codedev@gmail.com>
2025-07-31 15:41:16 +0530

    frist change

--- src/utils.ts
+++ src/utils.ts
@@ -11,15 +11,3 @@ export function formatTime(time: number) {
   const seconds = Math.floor(time % 60);
   return `${minutes}:${seconds.toString().padStart(2, '0')}`;
 }
-
-export function formatDate(date: Date) {
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false,
-  };
-  return new Intl.DateTimeFormat('en-US', options).format(date);
-}
